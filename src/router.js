9cd05c7a0f420cd6fe7ad113754c60804b47453d
horek <azenoxe@gmail.com>
2022-01-13 15:13:52 +0100

    fix: on class router replace currentUrl by currentHash

--- src/router.js
+++ src/router.js
@@ -3,18 +3,16 @@ const rootElem = document.getElementById("root");
 export class Router {
   constructor(routes) {
     this.routes = routes;
-    this.currentUrl = location.pathname.slice(1) + location.hash.slice(1) || '';
+    this.currentHash = location.pathname.slice(1) + location.hash.slice(1) || '';
     this.render();
     window.onhashchange = () => {
-      this.currentUrl = location.pathname.slice(1) + location.hash.slice(1) || '';
+      this.currentHash = location.pathname.slice(1) + location.hash.slice(1) || '';
       this.render();
     };
   }
 
   render() {
-      console.log(this.currentUrl)
-    const route = this.routes.find(route => route.hash === this.currentUrl);
-    console.log(location)
+    const route = this.routes.find(route => route.hash === this.currentHash);
     if (route) {
       if (rootElem.childNodes.length) {
         rootElem.replaceChild(route.component, rootElem.childNodes[0]);
