42e9eda7a576e988d7b18c619a9da06b347f8ee0
Oliwier Maziarz <maziarzoliwier@outlook.com>
2022-01-14 14:44:42 +0100

    add Component template

--- src/React/Component.js
+++ src/React/Component.js
@@ -1,3 +1,13 @@
 class Component {
+  currentProps = [];
+
+  display(newProps) {
+    if(shouldUpdate(newProps)) {}
+  }
+
+  shouldUpdate(newProps, currentProps) {
+    return newProps !== currentProps;
+  }
   
+  render() {}
 }
